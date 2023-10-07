07b3d98b008766a9c4fa0b904135dba710b8fc05
Muhammad Hashim <muhammadhashimsardar@gmail.com>
2023-10-07 19:24:16 +0500

    feat(categories): :triangular_flag_on_post: Add Delet button

--- src/Components/SideBar/sidebar.jsx
+++ src/Components/SideBar/sidebar.jsx
@@ -1,13 +1,7 @@
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuItem from "@mui/material/MenuItem";
-import { Link } from "react-router-dom";
+
 import "./sidebar.css";
+import { Link } from "react-router-dom";
 export default function SideBar() {
   return (
     <div className="sideBar">
